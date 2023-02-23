b869b52940cb15bfa9e93c27bd984af002aa3c18
souvikadhikary <souvik500>
2023-02-24 03:38:10 +0530

    Movie App

--- src/main/java/com/driver/MovieService.java
+++ src/main/java/com/driver/MovieService.java
@@ -1,4 +1,32 @@
 package com.driver;
 
-public class MovieService {
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService
+{
+    MovieRepository movieRepository;
+    public void addMovie (Movie movie) {movieRepository.saveMovies(movie);}
+
+    public void addDirector (Director director) { movieRepository.saveDirectory(director); }
+
+    public void addDirectorCreatMovie (String movie, String director) {
+        movieRepository.saveDirectorMoviePair(movie,director);
+    }
+
+    public Movie getMovies (String movie) {return movieRepository.findMovies(movie);}
+
+    public Director getDirectors (String director) {return movieRepository.findDirector(director);}
+
+    public List<String> getListOfMoviesFromDirector (String director) {
+        return movieRepository.findMoviesByDirector(director);
+    }
+
+    public List<String> getAllMovies () {return movieRepository.findAllMovies();}
+
+    public void deleteDirectors (String director) {movieRepository.deleteDirector(director);}
+
+    public void deleteAllDirectors() {movieRepository.deleteAllDirector();}
 }
